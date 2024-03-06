c8a6735e7aae066d45870869b7aa4120c8757417
MRMSANJAI <mrmsanjai@gmail.com>
2024-03-06 16:29:05 +0530

    decond commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
-
-import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import {BrowserRouter,Routes,Route}from 'react-router-dom';
+import Shop from './Pages/Shop';
+import Shopcategory from './Pages/Shopcategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
      <Navbar/>
+     <Routes>
+      <Route path='/'element={<Shop/>}/>
+      <Route path='/Mens'element={<Shopcategory category="men"/>}/>
+      <Route path='/Women'element={<Shopcategory category="women"/>}/>
+      <Route path='/Kids'element={<Shopcategory category="kid"/>}/>
+      <Route path='/Product'element={<Product/>}>
+       <Route path=':productID'element={<Product/>}/>
+     </Route>
+     <Route path='/cart'element={<Cart/>}/>
+     <Route path='/login'element={<LoginSignup/>}/>
+     </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/Components/Newcollections/Newcollections.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Newcollections.css';
+
+
+const Newcollections = () => {
+  return (
+    <div className="newcollections">
+        <h1>NEW COLLECTIONS</h1>
+        <hr/>
+        <div className="collections"></div>
+    </div>
+  )
+}
+
+export default Newcollections
